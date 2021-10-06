cd70079c0ce6ae5dc03fc3231492ec719e20ca87
Mohd Adil <adil88.mohd@gmail.com>
2021-10-06 20:49:47 +0530

    Initial Commit

---
+++ Voice Assistant.py
@@ -0,0 +1,105 @@
+import pyttsx3 #pip install pyttsx3
+import speech_recognition as sr #pip install speechRecognition
+import datetime
+import wikipedia #pip install wikipedia
+import webbrowser
+import wolframalpha
+
+engine = pyttsx3.init('sapi5')
+voices = engine.getProperty('voices')
+# print(voices[1].id)
+engine.setProperty('voice', voices[0].id)
+
+
+def speak(audio):
+    engine.say(audio)
+    engine.runAndWait()
+
+
+def wishMe():
+    hour = int(datetime.datetime.now().hour)
+    if hour>=0 and hour<12:
+        speak("Good Morning!")
+
+    elif hour>=12 and hour<18:
+        speak("Good Afternoon!")   
+
+    else:
+        speak("Good Evening!")  
+
+    speak("I am Jarvis Sir. Please tell me how may I help you")       
+
+def takeCommand():
+    #It takes microphone input from the user and returns string output
+
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        print("Listening...")
+        r.energy_threshold = 4000
+        audio = r.listen(source)
+
+    try:
+        print("Recognizing...")    
+        query = r.recognize_google(audio, language='en-in')
+        print(f"User said: {query}\n")
+
+    except Exception as e:
+        # print(e)    
+        print("Say that again please...")  
+        return "None"
+    return query
+
+
+if __name__ == "__main__":
+    wishMe()
+    while True:
+    # if 1:
+        query = takeCommand().lower()
+
+        # Logic for executing tasks based on query
+        if 'wikipedia' in query:
+            speak('Searching Wikipedia...')
+            query = query.replace("wikipedia", "")
+            results = wikipedia.summary(query, sentences=2)
+            speak("According to Wikipedia")
+            print(results)
+            speak(results)
+
+        elif 'open youtube' in query:
+            webbrowser.open("youtube.com")
+
+        elif 'who are you' in query:
+            speak("I am your voice assistant. How can I help You?")
+
+        elif 'your name' in query:
+            speak("I have no name sir.")
+
+        elif 'open google' in query:
+            webbrowser.open("google.com")
+
+        elif 'open stackoverflow' in query:
+            webbrowser.open("stackoverflow.com")   
+
+
+        elif 'the time' in query:
+            strTime = datetime.datetime.now().strftime("%H:%M:%S")    
+            speak(f"Sir, the time is {strTime}") 
+
+        elif 'answer me' in query:
+            speak('Please ask your question')
+            question = takeCommand()
+            try:
+                app_id = 'J2LR63-YLJKEHL9L8'
+                client = wolframalpha.Client('J2LR63-YLJKEHL9L8')
+                res = client.query(question)
+                answer = next(res.results).text
+                speak(answer)
+                print(answer)
+            except Exception as e:
+                print('Sorry. This is out of my reach')
+                speak('Sorry. This is out of my reach')
+
+
+        elif 'exit' in query:
+            speak("Ok, Goodbye")
+            break
